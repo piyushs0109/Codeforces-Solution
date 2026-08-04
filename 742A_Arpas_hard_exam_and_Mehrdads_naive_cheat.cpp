cf5a787259573fea8f0296199249cd2d62336115
Piyush Singh <piyushsingh221215@gmail.com>
2026-08-04 19:41:54 +0530

    Sync Codeforces: 742A - Arpa’s hard exam and Mehrdad’s naive cheat [C++17 (GCC 7-32)]

---
+++ 742A_Arpas_hard_exam_and_Mehrdads_naive_cheat.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    if(n ==0)cout<<1<<endl;
+    else{
+        n = n % 4 ;
+        if(n == 1)cout<<8<<endl;
+        if(n == 2)cout<<4<<endl;
+        if(n == 3)cout<<2<<endl;
+        if(n == 0)cout<<6<<endl;
+ 
+    }
+    return 0 ;
+}

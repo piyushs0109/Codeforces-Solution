09adfcc1aec9c8369d2b4e5dbb32da718da828e7
Piyush Singh <piyushsingh221215@gmail.com>
2026-08-14 00:22:31 +0530

    Sync Codeforces: 118A - String Task [C++17 (GCC 7-32)]

---
+++ 118A_String_Task.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    string s;
+    cin>>s;
+    for(int i =0;i<s.length();i++){
+        if(s[i]=='A' ||s[i]=='E' || s[i]=='I'|| s[i]=='O'|| s[i]=='U'|| s[i]=='a'|| s[i]=='e'|| s[i]=='i'|| s[i]=='o'|| s[i]=='u'|| s[i]=='Y'|| s[i]=='y' ){
+        
+        }
+        else cout<<"."<<(char)tolower(s[i]);
+    }
+ 
+}

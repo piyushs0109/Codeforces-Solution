75872400785b38cc5ec97a1528f299988e318248
Piyush Singh <piyushsingh221215@gmail.com>
2026-08-27 01:47:32 +0530

    Sync Codeforces: 749A - Bachgold Problem [C++17 (GCC 7-32)]

---
+++ 749A_Bachgold_Problem.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    long long n;
+    cin>>n;
+    if(n%2==0){
+        cout<<n/2<<endl;
+        for(int i =0;i<n/2;i++){
+            cout<<2<<" ";
+        }
+    }
+    else {
+        cout<<n/2<<endl;
+        for(int i =1;i<n/2;i++){
+            cout<<2<<" ";
+        }
+        cout<<3<<endl;
+    }
+}

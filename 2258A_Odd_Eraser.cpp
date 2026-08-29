2e3fde56696ea7bca2b69eee020a9ad8b57553c4
Piyush Singh <piyushsingh221215@gmail.com>
2026-08-29 21:02:30 +0530

    Sync Codeforces: 2258A - Odd Eraser [C++17 (GCC 7-32)]

---
+++ 2258A_Odd_Eraser.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i =0;i<n;i++){
+            cin>>a[i];
+        }
+        long long result = __gcd(a[0],a[n-1]);
+        cout<<result<<endl;
+    }
+    return 0;
+}
